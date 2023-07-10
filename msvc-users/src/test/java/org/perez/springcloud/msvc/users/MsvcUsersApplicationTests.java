7baa496d3c95aea30a7d7b5274981378096a333c
Jeremy_134 <u202022237@upc.edu.pe>
2023-07-10 16:08:25 -0500

    Initial commit

---
+++ msvc-users/src/test/java/org/perez/springcloud/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perez.springcloud.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
